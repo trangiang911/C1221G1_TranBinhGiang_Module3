bf40ef81596f34907001e93ad72e7fcbff0d6bbf
trangiang911 <tbgiang191199@gmail.com>
2022-03-28 17:14:07 +0700

    up git

--- ss1_co_so_du_lieu_quan_he/bai_tap/quan_ly_sinh_vien.sql
+++ ss1_co_so_du_lieu_quan_he/bai_tap/quan_ly_sinh_vien.sql
@@ -1,12 +1,12 @@
 create database `student-management`;
-create table `student-management`.`studen`(
+create table `student-management`.`student`(
 `id` int not null,
 `name` varchar(45) null,
 `age` int null,
 `country` varchar(45) null,
 primary key (`id`)); 
 create table `student-management`.`class`(
-`id` int primary key not null,
+`id` int  primary key not null,
 `nameclass` varchar(45) null,
 `amount` int null
 );
@@ -15,3 +15,15 @@ create table `student-management`.`teacher`(
 `name` varchar(45) null,
 `age` int null,
 `country` varchar(45) null );
+insert  into `student-management`.`class`
+(id,`nameclass`,amount)
+values
+(10,"C1221G1",27),
+(26,"C0322G1",20),
+(34,"C121G1",30),
+(47,"C022G1",50);
+
+alter table class
+add teacher_cn varchar(50);
+alter table class
+drop teacher_cn;
